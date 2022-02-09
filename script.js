05046d001c2848c70ade811dd8cb9401fa70cd6e
Wanny Martins <wcatery@gmail.com>
2022-02-09 19:34:03 -0300

    requisito 7

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
 const rgbColor = document.getElementById('rgb-color');
 const bntResetGame = document.getElementById('reset-game');
+const score = document.getElementById('score');
+const respostaInicial = 'Escolha uma cor';
+let resultPlacar = 0;
+
 
 const rgb = (a, b, c) => {
   a = Math.floor(Math.random() * 256);
@@ -27,6 +31,11 @@ const ballCerta = () => {
   rgbColor.innerText = de1A6.style.backgroundColor;
   return rgbColor.innerText;
 };
+const refresh = () => {
+  corAletoria();
+  ballCerta();
+  answer.innerText = respostaInicial;
+};
 
 ballCerta();
 answer.innerText = 'Escolha uma cor';
@@ -35,15 +44,20 @@ const textoResposta = (event) => {
   const click = event.target;
   console.log(click.style.backgroundColor);
   if (click.style.backgroundColor === rgbColor.innerText) {
-    answer.innerHTML = 'Acertou!';
+    resultPlacar += 3;
+    score.innerText = resultPlacar;
+    answer.innerText = 'Acertou!';
   } else {
     answer.innerHTML = 'Errou! Tente novamente!';
+    resultPlacar -= 1;
+    score.innerText = resultPlacar;
+    if (resultPlacar <= -6) {
+      resultPlacar = 0;
+      answer.innerHTML = 'Você Perdeu! Clique em Reiniciar o jogo!';
+      score.innerText = 0;
+    }
   }
 };
-
-const refresh = () => {
-  window.location.reload();
-}
 
 balls.forEach((ball) => ball.addEventListener('click', textoResposta));
 bntResetGame.addEventListener('click', refresh);
