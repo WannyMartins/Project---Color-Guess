41033416b480b0cc2e0e62d10282d2f503a1255d
Wanny Martins <wcatery@gmail.com>
2022-02-09 18:38:55 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const balls = document.querySelectorAll('.ball');
+const answer = document.getElementById('answer');
+const rgbColor = document.getElementById('rgb-color');
 
 const rgb = (a, b, c) => {
-    a = Math.floor(Math.random() * 256);
-    b = Math.floor(Math.random() * 256);
-    c = Math.floor(Math.random() * 256);
+  a = Math.floor(Math.random() * 256);
+  b = Math.floor(Math.random() * 256);
+  c = Math.floor(Math.random() * 256);
 
-    return `rgb(${a}, ${b}, ${c})`;
+  return `rgb(${a}, ${b}, ${c})`;
 }
 
 const corAletoria = () => {
-    balls.forEach((ball) => {
-        ball.style.backgroundColor = rgb();
-    })
+  balls.forEach((ball) => {
+    ball.style.backgroundColor = rgb();
+  })
+};
+
+corAletoria();
+
+const ballCerta = () => {
+  let de1A6 = balls.length;
+  de1A6 = Math.floor(Math.random() * 5);
+  console.log(de1A6)
+  de1A6 = balls[de1A6];
+  rgbColor.innerText = de1A6.style.backgroundColor;
+  return rgbColor.innerText;
 }
-corAletoria()
+console.log(ballCerta());
+
+answer.innerText = 'Escolha uma cor';
+
+const textoResposta = (event) => {
+  const click = event.target;
+  console.log(click.style.backgroundColor);
+  if (click.style.backgroundColor === rgbColor.innerText) {
+    answer.innerHTML = 'Acertou!';
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+  }
+};
+
+balls.forEach((ball) => ball.addEventListener('click', textoResposta))
