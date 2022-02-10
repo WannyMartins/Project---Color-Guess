2007ed8761a28f41afc8e1c809b7b67f113a37f5
Wanny Martins <wcatery@gmail.com>
2022-02-10 13:21:03 -0300

    finalizando estilização

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const answer = document.getElementById('answer');
 const rgbColor = document.getElementById('rgb-color');
 const bntResetGame = document.getElementById('reset-game');
 const score = document.getElementById('score');
-const respostaInicial = 'Escolha uma cor';
+
 let resultPlacar = 0;
 
-
-const rgb = (a, b, c) => {
-  a = Math.floor(Math.random() * 256);
-  b = Math.floor(Math.random() * 256);
-  c = Math.floor(Math.random() * 256);
+const rgb = () => {
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
 
   return `rgb(${a}, ${b}, ${c})`;
-}
+};
 
 const corAletoria = () => {
   balls.forEach((ball) => {
-    ball.style.backgroundColor = rgb();
+    const bolas = ball;
+    bolas.style.backgroundColor = rgb();
   });
 };
 
@@ -26,36 +26,48 @@ corAletoria();
 const ballCerta = () => {
   let de1A6 = balls.length;
   de1A6 = Math.floor(Math.random() * 5);
-  console.log(de1A6)
+  console.log(de1A6);
   de1A6 = balls[de1A6];
   rgbColor.innerText = de1A6.style.backgroundColor;
   return rgbColor.innerText;
 };
+
+const respostaInicial = () => {
+  answer.innerText = 'Escolha uma cor';
+};
+
+respostaInicial();
+
 const refresh = () => {
   corAletoria();
   ballCerta();
-  answer.innerText = respostaInicial;
+  respostaInicial();
 };
 
 ballCerta();
-answer.innerText = 'Escolha uma cor';
+
+const setTout = () => {
+  setTimeout(() => {
+    refresh();
+  }, 1000);
+};
 
 const textoResposta = (event) => {
   const click = event.target;
-  console.log(click.style.backgroundColor);
   if (click.style.backgroundColor === rgbColor.innerText) {
     resultPlacar += 3;
     score.innerText = resultPlacar;
     answer.innerText = 'Acertou!';
+    setTout();
   } else {
     answer.innerHTML = 'Errou! Tente novamente!';
-    // resultPlacar -= 1;
+    resultPlacar -= 1;
     score.innerText = resultPlacar;
-    // if (resultPlacar <= -6) {
-    //   resultPlacar = 0;
-    //   answer.innerHTML = 'Você Perdeu! Clique em Reiniciar o jogo!';
-    //   score.innerText = 0;
-    // }
+    if (resultPlacar <= -6) {
+      resultPlacar = 0;
+      answer.innerHTML = 'Você Perdeu! Clique em Reiniciar o jogo!';
+      score.innerText = 0;
+    }
   }
 };
 
